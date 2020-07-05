957c04ffd8fdfd2c4526026367813d7546c10dd1
dail11 <dail11@sany.com.cn>
2020-07-05 08:44:02 +0800

    init

---
+++ git-test-proj/src/com/dailing/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.dailing.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git proj");
+        System.out.println("hello git proj");
+        System.out.println("hello git proj");
+        System.out.println("hello git proj");
+        System.out.println("hello git proj");
+
+
+    }
+}
